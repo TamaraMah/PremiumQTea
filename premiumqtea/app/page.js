75198ca2c94c15f685c4cac336c5687176988218
TamaraMah <tamaramahmoud21@gmail.com>
2023-11-26 21:01:38 -0700

    meow

---
+++ premiumqtea/app/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function TitlePage() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold text-gray-800">Premium QTea</h1>
+    </main>
+  );
+}
