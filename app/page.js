08bbe2ff292d70880d256ca50624e213bdebcf7f
TamaraMah <tamaramahmoud21@gmail.com>
2023-12-08 02:08:17 -0700

    meow

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 
-export default function TitlePage() {
+export default function NavBar() {
   return (
-    <main className="bg-[url('/catrain.gif')] flex flex-col items-center justify-center h-screen bg-cover">
-      <h1 className="text-6xl font-bold text-white shadow-lg relative">Premium QTea</h1>
-    </main>
+  <div className="bg-white flex flex-col min-h-screen">
+    <nav className="bg-gray-100 shadow-md">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between items-center py-3">
+          {/* Logo and title */}
+          <Link href="/">
+            <div className="flex items-center text-gray-700 hover:text-gray-900 cursor-pointer">
+              {/* Replace /path-to-your-logo.svg with your actual logo path */}
+              <img src="/cat.jpg" alt="Premium QTea Logo" className="h-10 mr-3" />
+              <span className="font-bold text-2xl">Premium QTea</span>
+            </div>
+          </Link>
+          <div className="hidden md:flex items-center space-x-1">
+            <Link href="/shopall"><div className="py-5 px-3 text-gray-700 hover:text-gray-900 cursor-pointer">Shop All</div></Link>
+            <Link href="/aboutus"><div className="py-5 px-3 text-gray-700 hover:text-gray-900 cursor-pointer">About Us</div></Link>
+            <Link href="/contact"><div className="py-5 px-3 text-gray-700 hover:text-gray-900 cursor-pointer">Contact</div></Link>
+          </div>
+        </div> 
+      </div>
+    </nav>
+  </div>
+
   );
 }
